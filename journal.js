8a5af96c7f4372575f33913ce2573e157cd358c1
Peter Fleming <fleming_peter01@student.smc.edu>
2025-06-30 18:26:11 -0700

    adding function that converts meters to feet.

--- journal.js
+++ journal.js
@@ -13,3 +13,12 @@ function isEven(num) {
   return num % 2 === 0;
 }
 
+// this function calculates the wattage given volts and amps
+function calculateWattage(volts, amps) {
+  return volts * amps;
+}
+
+// this function converts meters to feet
+function metersToFeet(meters) {
+  return meters * 3.28084;
+}
